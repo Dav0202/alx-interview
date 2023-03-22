93382ac767e2a9601d337f0ab533eae5a72bc442
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-03-22 23:39:30 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,17 +2,17 @@
 """ Module for calculating Pascal Triangle """
 
 def pascal_triangle(n):
-    """ Function for creating a pascal triangle as a list of lists
-    """
-    if n <= 0:
-        return ([])
-
-    tmp = [[1]]
-    for i in range(1, n):
-        row = [1]
-        new = tmp[i - 1]
-        for u in range(len(new)):
-            new = new[u] + new[u + 1] if u != len(new) - 1 else 1
-            row.append(new)
-        tmp.append(row)
-    return tmp
+    '''
+    Pascal's triangle
+    '''
+    lists = []
+    if n == 0:
+        return lists
+    for i in range(n):
+        lists.append([])
+        lists[i].append(1)
+        if (i > 0):
+            for j in range(1, i):
+                lists[i].append(lists[i - 1][j - 1] + lists[i - 1][j])
+            lists[i].append(1)
+    return lists
