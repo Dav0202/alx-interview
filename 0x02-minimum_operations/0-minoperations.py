87d5b29939b59128468b10b83737e8e8d7d4e957
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-04-05 20:28:38 +0100

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,1 +1,21 @@
+#!/usr/bin/python3
 
+""" Minimum Operations """
+
+
+def minOperations(n):
+    """
+    min operation function
+    """
+    if not isinstance(n, int):
+        return 0
+
+    min_op = 0
+    char = 2
+    while (char <= n):
+        if not (n % char):
+            n = int(n / char)
+            min_op += char
+            char = 1
+        char += 1
+    return min_op
