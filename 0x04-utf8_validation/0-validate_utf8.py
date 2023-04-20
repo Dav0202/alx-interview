ff596fc5597b5bc1fcd50a81e6acae63d81fa998
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-04-20 08:12:21 +0100

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,1 +1,39 @@
+#!/usr/bin/python3
+""" UTF-8 Validation """
 
+
+def validUTF8(data):
+    """
+    Determine if a given data set represents a valid
+    UTF-8 encoding.
+    """
+    no_of_bytes = 0
+
+    mk1 = 1 << 7
+    mk2 = 1 << 6
+
+    for i in data:
+
+        mk_and_byte = 1 << 7
+
+        if no_of_bytes == 0:
+            while mk_and_byte & i:
+                no_of_bytes += 1
+                mk_and_byte = mk_and_byte >> 1
+
+            if no_of_bytes == 0:
+                continue
+
+            if no_of_bytes == 1 or no_of_bytes > 4:
+                return False
+
+        else:
+            if not (i & mk1 and not (i & mk2)):
+                    return False
+
+        no_of_bytes -= 1
+
+    if no_of_bytes == 0:
+        return True
+
+    return False
