1a90baa271e105b4bcbcd04ac978d6ec141630e7
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-05-04 15:07:29 +0100

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,1 +1,36 @@
-d
+#!/usr/bin/node
+const argv = process.argv;
+const film_url = 'https://swapi-api.alx-tools.com/api/films/';
+const movie_url = `${film_url}${argv[2]}/`;
+
+const request = require('request');
+
+request(movie_url, function (error, response, body) {
+  if (error == null) {
+    const parsed_body = JSON.parse(body);
+    const characters = parsed_body.characters;
+
+    if (characters && characters.length > 0) {
+      const limit = characters.length;
+      GetCharacter(0, characters[0], characters, limit);
+    }
+  } else {
+    console.log(error);
+  }
+});
+
+function GetCharacter (idx, url, characters, limit) {
+  if (idx === limit) {
+    return;
+  }
+  request(url, function (error, response, body) {
+    if (!error) {
+      const rbody = JSON.parse(body);
+      console.log(rbody.name);
+      idx++;
+      GetCharacter(idx, characters[idx], characters, limit);
+    } else {
+      console.error('error:', error);
+    }
+  });
+}
