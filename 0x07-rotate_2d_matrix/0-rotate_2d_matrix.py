573553f41deff7ee8ad3e29a6e565976e3cfa05f
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-05-11 16:03:18 +0100

    Update 0-rotate_2d_matrix.py
    implementation

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,1 +1,28 @@
+#!/usr/bin/python3
+""" Rotate a 2D matrixrix 90 Degrees Clockwise"""
 
+
+def rotate_2d_matrixrix(matrix):
+    """ Function to rotate a matrixrix 90 degree clockwise
+    """
+
+    # base case
+    if not matrixrix or not len(matrix):
+        return
+ 
+    # `N × N` matrixrix
+    N = len(matrix)
+ 
+    # Transpose the matrixrix
+    for i in range(N):
+        for j in range(i):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp
+ 
+    # swap columns
+    for i in range(N):
+        for j in range(N // 2):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[i][N - j - 1]
+            matrix[i][N - j - 1] = temp
