28d2fbcdbec984a459ba9df000f08508c1171051
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-05-19 22:54:20 +0100

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,1 +1,25 @@
+def makeChange(coins, total):
+    """
+    Given a pile of coins of different values, determine the fewest
+    number of coins needed to meet a given amount total.
+    """
+    if total <= 0:
+        return 0
 
+    i = total + 1
+
+    data = {0: 0}
+
+    for i in range(1, total + 1):
+        data[i] = i
+        for coin in coins:
+            current = i - coin
+            if current < 0:
+                continue
+            
+            data[i] = min(data[current] + 1, data[i])
+
+    if data[total] == total + 1:
+        return -1
+
+    return data[total]
